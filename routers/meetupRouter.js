9d7559b955a2f7132fc8e7df474a6aa8c1d3fef5
Vitalya238 <kirpichenko.2004@bk.ru>
2024-06-19 21:39:17 +0300

    DTO Validation(on create and update) via joi package

--- routers/meetupRouter.js
+++ routers/meetupRouter.js
@@ -1,50 +1,59 @@
 const express = require('express');
 const router = express.Router();
 const meetupController = require('../controllers/meetupController');
+const { createMeetupSchema, updateMeetupSchema } = require('../dto/meetup_dto');
 
 router.get('/', async (req, res) => {
     try {
-        await meetupController.getAllMeetup(req, res);
+      await meetupController.getAllMeetup(req, res);
     } catch (error) {
-        console.error('Error in getAllMeetup router:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+      console.error('Error in getAllMeetup router:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
     }
-});
-
-router.get('/:id', async (req, res) => {
+  });
+  
+  router.get('/:id', async (req, res) => {
     try {
-        await meetupController.getOneMeetup(req, res);
+      await meetupController.getOneMeetup(req, res);
     } catch (error) {
-        console.error('Error in getOneMeetup router:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+      console.error('Error in getOneMeetup router:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
     }
-});
-
-router.post('/', async (req, res) => {
+  });
+  
+  router.post('/', async (req, res) => {
     try {
-        await meetupController.createMeetup(req, res);
+      const { error } = createMeetupSchema.validate(req.body);
+      if (error) {
+        return res.status(400).json({ error: error.details[0].message });
+      }
+      await meetupController.createMeetup(req, res);
     } catch (error) {
-        console.error('Error in getAllMeetup router:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+      console.error('Error in createMeetup router:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
     }
-});
-
-router.put('/:id', async (req, res) => {
+  });
+  
+  router.put('/:id', async (req, res) => {
     try {
-        await meetupController.updateMeetup(req, res);
+      const { error } = updateMeetupSchema.validate(req.body);
+      if (error) {
+        return res.status(400).json({ error: error.details[0].message });
+      }
+      await meetupController.updateMeetup(req, res);
     } catch (error) {
-        console.error('Error in updateMeetup router:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+      console.error('Error in updateMeetup router:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
     }
-});
-
-router.delete('/:id', async (req, res) => {
+  });
+  
+  router.delete('/:id', async (req, res) => {
     try {
-        await meetupController.deleteMeetup(req, res);
+      await meetupController.deleteMeetup(req, res);
     } catch (error) {
-        console.error('Error in getAllMeetup router:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+      console.error('Error in deleteMeetup router:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
     }
-});
-
-module.exports = router;
+  });
+  
+  module.exports = router;
