701c6da20d431b883cb64822d27ba2c3679e74ea
Vitalya238 <kirpichenko.2004@bk.ru>
2024-06-20 16:48:42 +0300

    JS Core Advanced + first task use_strict added

---
+++ JS Core Advanced/use_strict.js
@@ -0,0 +1,26 @@
+"use strict";
+
+function factorial(n) {
+
+    if (typeof n !== "number") {
+        return;
+    }
+    if (n < 0) {
+        return;
+    }
+    if (n === 0 || n === 1) {
+        return 1;
+    }
+    let result = 1;
+    for (let i = 2; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+    
+}
+
+console.log(factorial("asd")); 
+console.log(factorial(0)); 
+console.log(factorial(-20)); 
+console.log(factorial(3)); 
+console.log(factorial(20)); 
