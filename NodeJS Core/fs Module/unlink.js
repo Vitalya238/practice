49d03c8fcb87a0d664b162dbf1c4ab7b6ef294d5
Vitalya238 <kirpichenko.2004@bk.ru>
2024-07-04 14:47:30 +0300

    fixed some mistakes

--- NodeJS Core/fs Module/unlink.js
+++ NodeJS Core/fs Module/unlink.js
@@ -10,22 +10,25 @@ function deleteTxtFiles() {
             console.error('Error reading directory:', err);
             return;
         }
+        const txtFiles = files.filter(file => path.extname(file).toLowerCase() === '.txt');
+        let deletedCount = 0;
         files.forEach(file => {
             if (path.extname(file).toLowerCase() === '.txt') {
                 fs.unlink(path.join(__dirname, file), (err => {
                     if (err) console.log(err);
                     else {
                         console.log(`Deleted file: ${path.join(__dirname, file)}`)
+                    }
+                    deletedCount++;
+                    if (deletedCount == txtFiles.length) {
                         getFilesInDirectory('after');
                     }
-
                 }));
             }
         });
     });
     getFilesInDirectory('before');
 }
-
 
 function getFilesInDirectory(status) {
     console.log(status);
